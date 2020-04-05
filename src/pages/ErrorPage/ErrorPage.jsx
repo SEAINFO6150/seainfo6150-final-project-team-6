f2fdf2d7f9f031e1f8c5477d823f8ac4b2477a71
fancytseng <55032412+fancytseng@users.noreply.github.com>
2020-04-04 23:23:15 -0700

    errorpage test

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import styles from "./ErrorPage.module.css"
 
 const ErrorPage = () => {
-  return <div>404 NOT FOUND....</div>;
-};
+  return (
+<div className={styles.error}>
 
-export default ErrorPage;
+  <section className={styles.container}>
+    <h1>
+      Oops Sorry!
+    </h1>
+    <h2>
+      404 Page not found
+    </h2>
+    <p>
+      It seems that something went wrong
+    </p>
+
+    <figure className={styles.imageContainer}>
+      <a href="/">
+        <img className={styles.image} src="https://cdn.pixabay.com/photo/2017/06/08/17/32/not-found-2384304__480.jpg" alt=""/>
+      </a>
+      <p>
+        <span>The receipe you are trying to search is not availble yet :( </span>
+      </p>
+    </figure>
+  </section>
+</div>
+
+)
+}
+export default ErrorPage
