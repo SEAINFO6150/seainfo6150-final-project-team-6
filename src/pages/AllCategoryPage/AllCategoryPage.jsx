3d78dd5b069cdf244bb7c3f979de2ec31d2d2790
victoriahao <mwei5669@gmail.com>
2020-04-05 16:48:12 -0700

    style css in AllCategoryPage

--- src/pages/AllCategoryPage/AllCategoryPage.jsx
+++ src/pages/AllCategoryPage/AllCategoryPage.jsx
@@ -1,23 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styles from "./AllCategoryPage.module.css";
 
 const AllCategoryPage = () => {
   return (
     <div>
-      <Link to="/category/1">
-        <button>Soup</button>
+      <Link className={styles.soupcategory} to="/category/1">
+        <button className={styles.soupcategorybutton}>Soup</button>
       </Link>
-      <Link to="/category/2">
-        <button>Appetizer</button>
+      <Link className={styles.appetizercategory} to="/category/2">
+        <button className={styles.appetizercategorybutton}>Appetizer</button>
       </Link>
-      <Link to="/category/3">
-        <button>Main Course</button>
+      <Link className={styles.maincoursecategory} to="/category/3">
+        <button className={styles.maincoursecategorybutton}>Main Course</button>
       </Link>
-      <Link to="/category/4">
-        <button>Snack</button>
+      <Link className={styles.snackcategory} to="/category/4">
+        <button className={styles.snackcategorybutton}>Snack</button>
       </Link>
-      <Link to="/category/5">
-        <button>Dessert</button>
+      <Link className={styles.dessertcategory} to="/category/5">
+        <button className={styles.dessertcategorybutton}>Dessert</button>
       </Link>
     </div>
   );
