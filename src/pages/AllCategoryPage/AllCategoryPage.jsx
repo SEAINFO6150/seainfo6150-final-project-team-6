5be190dbe4f4288a0b69edf32129262715614270
victoriahao <mwei5669@gmail.com>
2020-04-05 18:07:58 -0700

    edited styles className

--- src/pages/AllCategoryPage/AllCategoryPage.jsx
+++ src/pages/AllCategoryPage/AllCategoryPage.jsx
@@ -5,20 +5,20 @@ import styles from "./AllCategoryPage.module.css";
 const AllCategoryPage = () => {
   return (
     <div>
-      <Link className={styles.soupcategory} to="/category/1">
-        <button className={styles.soupcategorybutton}>Soup</button>
+      <Link className={styles.soupCategory} to="/category/1">
+        <button className={styles.soupCategoryButton}>Soup</button>
       </Link>
-      <Link className={styles.appetizercategory} to="/category/2">
-        <button className={styles.appetizercategorybutton}>Appetizer</button>
+      <Link className={styles.appetizerCategory} to="/category/2">
+        <button className={styles.appetizerCategoryButton}>Appetizer</button>
       </Link>
-      <Link className={styles.maincoursecategory} to="/category/3">
-        <button className={styles.maincoursecategorybutton}>Main Course</button>
+      <Link className={styles.mainCourseCategory} to="/category/3">
+        <button className={styles.mainCourseCategoryButton}>Main Course</button>
       </Link>
-      <Link className={styles.snackcategory} to="/category/4">
-        <button className={styles.snackcategorybutton}>Snack</button>
+      <Link className={styles.snackCategory} to="/category/4">
+        <button className={styles.snackCategoryButton}>Snack</button>
       </Link>
-      <Link className={styles.dessertcategory} to="/category/5">
-        <button className={styles.dessertcategorybutton}>Dessert</button>
+      <Link className={styles.dessertCategory} to="/category/5">
+        <button className={styles.dessertCategoryButton}>Dessert</button>
       </Link>
     </div>
   );
