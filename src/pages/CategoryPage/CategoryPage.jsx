f306ef626ff92bdeea0cd548eb869a151fa6dc03
cuifangyu <fangyu.cui0109@gmail.com>
2020-04-11 23:03:48 -0700

    recipes&categorys

--- src/pages/CategoryPage/CategoryPage.jsx
+++ src/pages/CategoryPage/CategoryPage.jsx
@@ -6,20 +6,40 @@ const getRecipePreview = (recipe, categoryID) => {
   if (recipe.categoryID === categoryID) {
     return (
       <div>
-        <Link to={recipe.detailURL}>
-          <p className={styles.soupRecipeName}>{recipe.name}</p>
+        <Link className={styles.categoryCard} to={recipe.detailURL}>
+          <img
+            src={recipe.imageURL}
+            alt={recipe.name}
+            className={styles.categoryRecipeImg}
+          />
+          <div className={styles.categoryRecipe}>
+            <p className={styles.categoryRecipeName}>{recipe.name}</p>
+            <p className={styles.categoryRecipeDescription}>
+              {recipe.description}
+            </p>
+          </div>
         </Link>
-        <p className={styles.soupRecipeDescription}>{recipe.description}</p>
       </div>
     );
   }
 };
 
-const CategoryPage = props => {
+const CategoryPage = (props) => {
   return (
-    <div>
-      {props.content.map(recipe => getRecipePreview(recipe, props.categoryID))}
-      <Link to="/category">Back to All Category Page</Link>
+    <div className={styles.categoryContainer}>
+      <Link className={styles.categoryBackLink} to="/category">
+        &lt; Back to All Category Page
+      </Link>
+      {props.content.map((recipe) =>
+        getRecipePreview(recipe, props.categoryID)
+      )}
+      <p className={styles.categoryText}>
+        More Recipes in the Future!{" "}
+        <Link className={styles.categorySubscribe} to="/subscribe">
+          Subscribe
+        </Link>{" "}
+        our Website to see the Updates!
+      </p>
     </div>
   );
 };
