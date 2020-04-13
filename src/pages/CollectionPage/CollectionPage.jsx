b6957162a6b7eac3fd01613dda4befe2615a0604
cuifangyu <38642590+cuifangyu@users.noreply.github.com>
2020-04-13 16:37:56 -0700

    homepage & collection page (#6)

--- src/pages/CollectionPage/CollectionPage.jsx
+++ src/pages/CollectionPage/CollectionPage.jsx
@@ -1,12 +1,38 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./CollectionPage.module.css";
 
-const CollectionPage = props => {
+const getRecipePreview = (recipe) => {
   return (
     <div>
-      My Collection
-      <h1>{props.content.title}</h1>
-      Category: {props.content.category}
-      <p>{props.content.detail}</p>
+      <Link className={styles.collectionCard} to={recipe.detailURL}>
+        <img
+          src={recipe.imageURL}
+          alt={recipe.name}
+          className={styles.collectionRecipeImg}
+        />
+        <div className={styles.collectionRecipe}>
+          <p className={styles.collectionRecipeName}>{recipe.name}</p>
+          <p className={styles.collectionRecipeDescription}>
+            {recipe.description}
+          </p>
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+const CollectionPage = (props) => {
+  return (
+    <div className={styles.collectionContainer}>
+      {props.content.map((recipe) => getRecipePreview(recipe))}
+      <p className={styles.collectionText}>
+        More Recipes in the Future!&nbsp;
+        <Link className={styles.collectionSubscribe} to="/subscribe">
+          Subscribe
+        </Link>
+        &nbsp;our Website to see the Updates!
+      </p>
     </div>
   );
 };
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,14 +3,18 @@ import styles from "./HomePage.module.css";
 
 const HomePage = () => {
   return (
-    <div>
-    
-      <p className={styles.homePageDetail}>
-        Welcome to our site! After exploring our recipes, hope you'll love to cook/bake even more.
+    <div className={styles.homeContainer}>
+      <img
+        className={styles.homeImg}
+        src="/images/homepage.jpg"
+        alt="homepage"
+      />
+      <h1 className={styles.homeTitle}>Welcome to HOME CHEF!!!</h1>
+      <p className={styles.homeText}>
+        We have everything you need to improve the life in the kitchen! After
+        exploring our recipes, hope you'll love to cook/bake even more.
       </p>
-
-      <img src="/images/homepage.jpg" alt="homepage" />
-
+      <p className={styles.homeText}>Explore our website and be a HOME CHEF!</p>
     </div>
   );
 };
