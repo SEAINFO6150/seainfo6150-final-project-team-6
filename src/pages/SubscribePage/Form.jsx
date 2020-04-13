5c33a64bff7a56d603dfc03725bcf01f768c30f7
Jayven <yin.jiaw@husky.neu.edu>
2020-04-13 16:12:41 -0700

    “subsribepage
    git commit -m “subsribepage

--- src/pages/SubscribePage/Form.jsx
+++ src/pages/SubscribePage/Form.jsx
@@ -1,58 +1,83 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import AboutPage from "../AboutPage/AboutPage";
+import styles from "../CategoryPage/CategoryPage.module.css";
 
 export default class Form extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      submittedForm: null
+    constructor(props) {
+        super(props);
+        this.state = {
+            submittedForm: null
+        };
+    }
+
+    onSubmit = async e => {
+        e.preventDefault();
+        const data = new FormData(e.target);
+        this.setState({submittedForm: data});
     };
-  }
 
-  onSubmit = async e => {
-    e.preventDefault();
-    const data = new FormData(e.target);
-    this.setState({ submittedForm: data });
-  };
+    render() {
+        return this.state.submittedForm ? (
+            <div>
+                <label htmlFor="entry">
+                    <ins>Thank you for your Subscribe! You are all set.<br/>
+                        And your form entry was
+                    </ins>
+                </label>
+                <p>{this.state.submittedForm.get("name")}</p>
+                <p> {this.state.submittedForm.get("email")}</p>
+                <p> {this.state.submittedForm.get("tel")}</p>
+            </div>
+        ) : (
+            <form onSubmit={this.onSubmit}>
 
-  render() {
-    return this.state.submittedForm ? (
-      <div>Your form entry was
-        <p>{this.state.submittedForm.get("name")}</p>
-        <p>{this.state.submittedForm.get("email")}</p>
-        <p>{this.state.submittedForm.get("tel")}</p>
-      </div>
-    ) : (
-      <form onSubmit={this.onSubmit}>
-        <p>Name : <input type="text" name="name"/><br/>
-        Email : <input type="email" name="email"/><br/>
-        SMS Text : <input type="tel" id="tel" name="tel"/></p>
-        <p>
-          Choose recipe type you like most : <br/>
-          <input type="radio" name="recipeType" value="Soup"/> Soup
-          <input type="radio" name="recipeType" value="Appetizer"/> Appetizer
-          <input type="radio" name="recipeType" value="MainCourse"/>Main Course
-          <input type="radio" name="recipeType" value="Snack"/> Snack
-          <input type="radio" name="recipeType" value="Dessert"/> Dessert
-        </p>
-        <p>
-          How can we reach you best ?
-          <select id="type" name="type">
-          <option>Email</option>
-          <option>SMS</option>
-        </select></p>
-        <p>
-          Language prefer :
-          <select id="language" name="language">
-            <option>English</option>
-            <option>Spanish</option>
-            <option>Chinese</option>
-            <option>French</option>
-            <option>Russian</option>
-            <option>Arabic</option>
-          </select></p>
-            <p><input type="submit" value="Send it" /></p>
-      </form>
-    );
-  }
+                <p>
+                    <label htmlFor="name">Name :</label><br/>
+                    <input type="name" name="name" placeholder="Your Name.."/>
+                </p>
+                <p>
+                    <label htmlFor="Email">Email :</label><br/>
+                    <input type="email" name="email" placeholder="Your Email.."/>
+                </p>
+                <p>
+                    <label htmlFor="Phone"> SMS Text :</label><br/>
+                    <input type="tel" id="tel" name="tel" placeholder="Your Phone Number.."/>
+                </p>
+                <p>
+                    <label htmlFor="reach"> How can we reach you best ?</label>
+                    <select id="type" name="type">
+                        <option value="Email">Email</option>
+                        <option value="SMS">SMS</option>
+                    </select>
+                </p>
+                <p>
+                    <label htmlFor="language">Language prefer :</label>
+                    <select id="language" name="language">
+                        <option value="English">English</option>
+                        <option value="Spanish">Spanish</option>
+                        <option value="Chinese">Chinese</option>
+                        <option value="French">French</option>
+                        <option value="Russian">Russian</option>
+                        <option value="Arabic">Arabic</option>
+                    </select>
+                </p>
+                <p>
+                    <label htmlFor="recipeType">Choose recipe type you like most : </label><br/>
+                    <input type="radio" name="recipeType" value="Soup"/>
+                    <label htmlFor="type1">Soup </label>
+                    <input type="radio" name="recipeType" value="Appetizer"/>
+                    <label htmlFor="type2">Appetizer </label>
+                    <input type="radio" name="recipeType" value="MainCourse"/>
+                    <label htmlFor="type3">Main Course </label>
+                    <input type="radio" name="recipeType" value="Snack"/>
+                    <label htmlFor="type4">Snack </label>
+                    <input type="radio" name="recipeType" value="Dessert"/>
+                    <label htmlFor="type5">Dessert </label>
+                </p>
+                <p>
+                    <input type="Submit" value="Send it"/>
+                </p>
+            </form>
+        );
+    }
 }
--- src/pages/SubscribePage/SubscribePage.jsx
+++ src/pages/SubscribePage/SubscribePage.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Form from "./Form.jsx";
+import styles from "./SubscribePage.module.css"
 
 const SubscribePage = () => {
   return (
-    <div>
-        <h1>Here comes new recipes!</h1>
-        <h1>Sounds exciting, right?</h1>
-        <p>If you like our recipe, please fill out the form below and click "Subscribe"</p>
+    <div className={styles.subscribe}>
+        <section className={styles.container}>
+        <h1>Here comes new recipes!<br/>
+            Sounds exciting, right?</h1>
+            <p><i>If you like our recipe, please fill out the form below and click "Subscribe"</i></p>
       <Form />
+        </section>
     </div>
   );
 };
