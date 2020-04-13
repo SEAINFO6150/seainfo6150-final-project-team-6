3dd53b98c95df26deb909abf4882164ea6373400
victoriahao <mwei5669@gmail.com>
2020-04-13 15:51:58 -0700

    update HomePage html and add css

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import styles from "./HomePage.module.css";
 
 const HomePage = () => {
   return (
     <div>
+    
+      <p className={styles.homePageDetail}>
+        Welcome to our site! After exploring our recipes, hope you'll love to cook/bake even more.
+      </p>
+
       <img src="/images/homepage.jpg" alt="homepage" />
+
     </div>
   );
 };
